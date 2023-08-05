871678e1fd313a27510eb879250d2c6fc1f81bd4
SimonNordon4 <simon.nordon@protonmail.com>
2023-08-05 17:03:23 +1000

    seperating into better methods.

--- SerializeInterfaceGenerator/SerializeInterfaceGenerator/SerializeInterfaceGenerator.cs
+++ SerializeInterfaceGenerator/SerializeInterfaceGenerator/SerializeInterfaceGenerator.cs
@@ -19,7 +19,7 @@ using System;
 internal class SerializeInterfaceAttribute : Attribute
 {
 }";
-    
+
     public void Initialize(GeneratorInitializationContext context)
     {
         context.RegisterForPostInitialization
@@ -30,7 +30,8 @@ internal class SerializeInterfaceAttribute : Attribute
     public void Execute(GeneratorExecutionContext context)
     {
         GenerateClasses(context);
-    } 
+    }
+
     private static void GenerateClasses(GeneratorExecutionContext context)
     {
         if (!(context.SyntaxReceiver is SyntaxReceiver receiver))
@@ -38,164 +39,204 @@ internal class SerializeInterfaceAttribute : Attribute
 
         foreach (var classDeclaration in receiver.Classes)
         {
-            var interfaces = new HashSet<string>();
-
-            var model = context.Compilation.GetSemanticModel(classDeclaration.SyntaxTree);
-            var fields = classDeclaration.DescendantNodes().OfType<FieldDeclarationSyntax>()
-                .Where(f => f.AttributeLists.Any(
-                    a => a.Attributes.Any(at => at.Name.ToString() == "SerializeInterface")));
-
-            if (!fields.Any())
-                continue;
-            
-            var namespaceDeclaration = classDeclaration.AncestorsAndSelf().OfType<NamespaceDeclarationSyntax>()
-                .FirstOrDefault();
-            var namespaceName = namespaceDeclaration?.Name.ToString();
-
-            var source = new StringBuilder();
-            source.AppendLine("using UnityEngine;");
-            // This namespace contains our ValidateInterface Attribute and Drawer.
-            source.AppendLine("using SerializeInterface;");
-            
-            // If the original class is a namespace, we want the generated class to be apart of the same namespace.
-            if (!string.IsNullOrEmpty(namespaceName))
-            {
-                source.Append($@"namespace {namespaceName}");
-                source.AppendLine("{");
-            }
-            
-            source.AppendLine($"public partial class {classDeclaration.Identifier.Text} : MonoBehaviour, ISerializationCallbackReceiver");
-            source.AppendLine("{");
-
-            // Generate the concrete backing fields for each interface.
-            foreach (var field in fields)
-            {
-                // Get the in script symbol for the field
-                var symbol = model.GetDeclaredSymbol(field.Declaration.Variables.First()) as IFieldSymbol;
-                // Get the full name of the interface
-                var interfaceNamespace = symbol.Type.ContainingNamespace.ToDisplayString();
-                // If the interface is in a namespace, we need to include it in the full name
-                var interfaceFullName =
-                    !string.IsNullOrEmpty(interfaceNamespace) && interfaceNamespace != "<global namespace>"
-                        ? interfaceNamespace + "." + symbol.Type.Name
-                        : symbol.Type.Name;
-                
-                // Gather all attributes on the original [SerializeInterface]
-                StringBuilder attributeStringBuilder = new StringBuilder();
-
-                // Loop through each attribute list in the field declaration
-                foreach (var attributeList in field.AttributeLists)
-                {
-                    // Loop through each attribute in the attribute list
-                    foreach (var attribute in attributeList.Attributes)
-                    {
-                        // If the attribute is not SerializeInterface, append it to the attribute string builder
-                        if (attribute.Name.ToString() != "SerializeInterface")
-                        {
-                            // Get the arguments and convert them to string with correct format
-                            var arguments = attribute.ArgumentList.Arguments
-                                .Select(arg => arg.ToString())
-                                .Aggregate((a, b) => a + ", " + b);
-
-                            // Append attribute to the string builder with arguments
-                            attributeStringBuilder.Append($"[{attribute.Name}({arguments})]");
-                        }
-                    }
-                }
-
-                
-                source.AppendLine(
-                    $"    [SerializeField, ValidateInterface(typeof({interfaceFullName}))]{attributeStringBuilder.ToString()} private Object {field.Declaration.Variables.First().Identifier.Text}_Object;");
-                //source.AppendLine($"    [SerializeField, ValidateInterface(typeof({interfaceFullName}))] private Object {field.Declaration.Variables.First().Identifier.Text}_Object;");
-                
-                interfaces.Add(interfaceFullName);
-            }
-
-            source.AppendLine("    void ISerializationCallbackReceiver.OnBeforeSerialize()");
-            source.AppendLine("    {");
-            source.AppendLine("    }");
-            
-            // We have to implement this because Unity didn't follow SOLID.
-            source.AppendLine("    void ISerializationCallbackReceiver.OnAfterDeserialize()");
-            source.AppendLine("    {");
-            
-            // Generate the code to assign the backing fields to the interface fields on BeforeSerialize
-            foreach (var field in fields)
-            {
-                var symbol = model.GetDeclaredSymbol(field.Declaration.Variables.First()) as IFieldSymbol;
-                source.AppendLine(
-                    $"        {field.Declaration.Variables.First().Identifier.Text} = {field.Declaration.Variables.First().Identifier.Text}_Object as {symbol.Type};");
-            }
-            source.AppendLine("    }");
-            
-            // Now add the InstantiateInterface method for each interface
-            foreach (var @interface in interfaces)
-            {
-                // Generate the method for the interface
-                source.AppendLine($"    private {@interface} InstantiateInterface({@interface} instance)");
-                source.AppendLine("    {");
-                source.AppendLine("        if (instance is MonoBehaviour monoInterface)");
-                source.AppendLine($"            return Object.Instantiate(monoInterface) as {@interface};");
-                source.AppendLine("        if (instance == null)");
-                source.AppendLine("        {");
-                source.AppendLine("            Debug.LogError(\"Attempted to instantiate interface with null instance!\", this);");
-                source.AppendLine("            return null;");
-                source.AppendLine("        }");
-                source.AppendLine($"        Debug.LogError($\"Attempted to instantiate interface {@interface}, but it is not a MonoBehaviour!\", this);");
-                source.AppendLine("        return null;");
-                source.AppendLine("    }");
-            }
-            
-            source.AppendLine("}");
-
-            // If the original class was part of the namespace, we have to close off the scope.
-            if (!string.IsNullOrEmpty(namespaceName))
-            {
-                source.AppendLine("}");
-            }
-            
-            // PrintOutputToPath(source, classDeclaration.Identifier.Text);
-            
-            context.AddSource($"{classDeclaration.Identifier.Text}_g.cs",
-                SourceText.From(source.ToString(), Encoding.UTF8));
+            GenerateClass(context, classDeclaration);
         }
-
-
     }
 
     /// <summary>
     /// Generate a partial class when detecting a [SerializeInterface]
     /// </summary>
-    private static void GenerateClass()
+    private static void GenerateClass(GeneratorExecutionContext context, ClassDeclarationSyntax classDeclaration)
     {
-        
+        HashSet<string> interfaces = new HashSet<string>();
+
+        SemanticModel model = context.Compilation.GetSemanticModel(classDeclaration.SyntaxTree);
+
+        FieldDeclarationSyntax[] fields = classDeclaration
+            .DescendantNodes()
+            .OfType<FieldDeclarationSyntax>()
+            .Where(f =>
+                f.AttributeLists
+                    .Any(a =>
+                        a.Attributes
+                            .Any(at => at.Name.ToString() == "SerializeInterface")))
+            .ToArray();
+
+        // If no [SerializeInterface] attribute is found, skip this class.
+        if (!fields.Any()) return;
+
+        NamespaceDeclarationSyntax namespaceDeclaration = classDeclaration
+            .AncestorsAndSelf()
+            .OfType<NamespaceDeclarationSyntax>()
+            .FirstOrDefault();
+        string namespaceName = namespaceDeclaration?.Name.ToString();
+
+        var source = new StringBuilder();
+
+        source.AppendLine("using UnityEngine;");
+        source.AppendLine("using SerializeInterface;");
+
+        // If the original class is a namespace, we want the generated class to be apart of the same namespace.
+        if (!string.IsNullOrEmpty(namespaceName))
+        {
+            source.AppendLine($@"namespace {namespaceName}");
+            source.AppendLine("{");
+        }
+
+        source.AppendLine(
+            $"public partial class {classDeclaration.Identifier.Text} : MonoBehaviour, ISerializationCallbackReceiver");
+        source.AppendLine("{");
+
+        // Generate the concrete backing fields for each interface.
+        foreach (var field in fields)
+        {
+            // Get the in script symbol for the field
+            IFieldSymbol symbol = model.GetDeclaredSymbol(field.Declaration.Variables.First()) as IFieldSymbol;
+
+            // Check if the field is a list of interfaces
+            bool isList = symbol?.Type is INamedTypeSymbol namedType &&
+                          namedType.IsGenericType &&
+                          namedType.Name == "List" &&
+                          namedType.TypeArguments.Length == 1 &&
+                          namedType.TypeArguments[0].TypeKind == TypeKind.Interface;
+
+
+            source.Append(
+                isList
+                    ? GenerateBackingObjectList(field, symbol)
+                    : GenerateBackingObject(field, symbol));
+
+
+            interfaces.Add(interfaceFullName);
+        }
+
+        source.AppendLine("    void ISerializationCallbackReceiver.OnBeforeSerialize()");
+        source.AppendLine("    {");
+        source.AppendLine("    }");
+
+        // We have to implement this because Unity didn't follow SOLID.
+        source.AppendLine("    void ISerializationCallbackReceiver.OnAfterDeserialize()");
+        source.AppendLine("    {");
+
+        // Generate the code to assign the backing fields to the interface fields on BeforeSerialize
+        foreach (var field in fields)
+        {
+            var symbol = model.GetDeclaredSymbol(field.Declaration.Variables.First()) as IFieldSymbol;
+            source.AppendLine(
+                $"        {field.Declaration.Variables.First().Identifier.Text} = {field.Declaration.Variables.First().Identifier.Text}_Object as {symbol.Type};");
+        }
+
+        source.AppendLine("    }");
+
+        // Now add the InstantiateInterface method for each interface
+        foreach (var @interface in interfaces)
+        {
+            // Generate the method for the interface
+            source.AppendLine($"    private {@interface} InstantiateInterface({@interface} instance)");
+            source.AppendLine("    {");
+            source.AppendLine("        if (instance is MonoBehaviour monoInterface)");
+            source.AppendLine($"            return Object.Instantiate(monoInterface) as {@interface};");
+            source.AppendLine("        if (instance == null)");
+            source.AppendLine("        {");
+            source.AppendLine(
+                "            Debug.LogError(\"Attempted to instantiate interface with null instance!\", this);");
+            source.AppendLine("            return null;");
+            source.AppendLine("        }");
+            source.AppendLine(
+                $"        Debug.LogError($\"Attempted to instantiate interface {@interface}, but it is not a MonoBehaviour!\", this);");
+            source.AppendLine("        return null;");
+            source.AppendLine("    }");
+        }
+
+        source.AppendLine("}");
+
+        // If the original class was part of the namespace, we have to close off the scope.
+        if (!string.IsNullOrEmpty(namespaceName))
+        {
+            source.AppendLine("}");
+        }
+
+        // PrintOutputToPath(source, classDeclaration.Identifier.Text);
+
+        context.AddSource($"{classDeclaration.Identifier.Text}_g.cs",
+            SourceText.From(source.ToString(), Encoding.UTF8));
     }
+
 
     /// <summary>
     /// Generate a back object to match an Interface.
     /// </summary>
     /// <returns></returns>
-    private static string GenerateBackingObject()
+    private static string GenerateBackingObject(FieldDeclarationSyntax field, IFieldSymbol symbol)
     {
-        return null;
-    }
-    
-    /// <summary>
-    /// Generate a backing Object List to match an Interface List.
-    /// </summary>
-    /// <returns>Generate a backing field of a List of Objects.</returns>
-    private static string GenerateBackingObjectList()
-    {
-        return null;
+        var interfaceFullName = GetInterfaceFullName(symbol);
+
+        // Gather all attributes on the original [SerializeInterface]
+        StringBuilder attributeStringBuilder = new StringBuilder();
+
+        // Loop through each attribute list in the field declaration
+        foreach (var attributeList in field.AttributeLists)
+        {
+            var inheritedAttributeList = GenerateInheritedAttributes(attributeList);
+            attributeStringBuilder.Append(inheritedAttributeList);
+        }
+
+        return
+            $"    [SerializeField, ValidateInterface(typeof({interfaceFullName}))]{attributeStringBuilder} private Object {field.Declaration.Variables.First().Identifier.Text}_SerializedObject;";
     }
 
     /// <summary>
-    /// Collect all attributes from the original interface field to be appended to the new backing field.
+    /// Generate a backing Object List to match an Interface List.
     /// </summary>
-    /// <returns>A string of attributes including their arguments.</returns>
-    private static string GenerateInheritedAttributes()
+    /// <returns></returns>
+    private static string GenerateBackingObjectList(FieldDeclarationSyntax field, IFieldSymbol symbol)
     {
-        return null;
+        var interfaceFullName = GetInterfaceFullName(symbol);
+
+        // Gather all attributes on the original [SerializeInterface]
+        StringBuilder attributeStringBuilder = new StringBuilder();
+
+        // Loop through each attribute list in the field declaration
+        foreach (var attributeList in field.AttributeLists)
+        {
+            var inheritedAttributeList = GenerateInheritedAttributes(attributeList);
+            attributeStringBuilder.Append(inheritedAttributeList);
+        }
+
+        return
+            $"    [SerializeField, ValidateInterface(typeof({interfaceFullName}))]{attributeStringBuilder} private List<Object> {field.Declaration.Variables.First().Identifier.Text}_SerializedObjectList;";
+    }
+
+    /// <summary>
+    /// Generates a string representation of inherited attributes from a given attribute list.
+    /// </summary>
+    /// <param name="attributeList">The list of attributes to process, e.g. [Range(), Tooltip].</param>
+    /// <returns>A string representation of inherited attributes.</returns>
+    private static string GenerateInheritedAttributes(AttributeListSyntax attributeList)
+    {
+        var inheritedAttributesList = new StringBuilder();
+    
+        // Loop through each attribute in the attribute list
+        foreach (var attribute in attributeList.Attributes)
+        {
+            var inheritedAttribute = GenerateInheritedAttributes(attribute);
+            inheritedAttributesList.Append(inheritedAttribute);
+        }
+
+        return inheritedAttributesList.ToString();
+    }
+
+    private static string GenerateInheritedAttributes(AttributeSyntax attribute)
+    {
+        // If the attribute is not SerializeInterface, append it to the attribute string builder
+        if (attribute.Name.ToString() == "SerializeInterface") return "";
+
+        // Get the arguments and convert them to string with correct format
+        var arguments = attribute.ArgumentList?.Arguments
+            .Select(arg => arg.ToString())
+            .Aggregate((a, b) => a + ", " + b);
+
+        return arguments == null ? $"[{attribute.Name}]" : $"[{attribute.Name}({arguments})]";
     }
 
     /// <summary>
@@ -224,8 +265,26 @@ internal class SerializeInterfaceAttribute : Attribute
     {
         return null;
     }
-    
-    
+
+    /// <summary>
+    /// Generate the the full name of the symbol, including optional namespace.
+    /// </summary>
+    /// <param name="symbol"></param>
+    /// <returns></returns>
+    private static string GetInterfaceFullName(IFieldSymbol symbol)
+    {
+        // Get the full name of the interface
+        string interfaceNamespace = symbol?.Type.ContainingNamespace.ToDisplayString();
+        // If the interface is in a namespace, we need to include it in the full name
+        string interfaceFullName =
+            !string.IsNullOrEmpty(interfaceNamespace) && interfaceNamespace != "<global namespace>"
+                ? interfaceNamespace + "." + symbol.Type.Name
+                : symbol?.Type.Name;
+
+        return interfaceFullName;
+    }
+
+
     private static void PrintOutputToPath(StringBuilder source, string classId)
     {
         var codeBase = Assembly.GetExecutingAssembly().CodeBase;
