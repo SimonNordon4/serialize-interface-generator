96f2f0d8e978b030e5d627edb6cd9cffa7658a1e
SimonNordon4 <simon.nordon@protonmail.com>
2023-08-09 15:02:18 +1000

    undoing bad implementation for reactive system

---
+++ Unity_SerializeInterfaceGenerator/Assets/ReactiveSystem/IntSystem.cs
@@ -0,0 +1,15 @@
+﻿    using UniRx;
+    using UnityEngine;
+
+    public partial class IntSystem : ReactiveSystem<int>, IInt
+    {
+        public IReadOnlyReactiveProperty<int> Value => property;
+        
+        
+        private int x;
+    }
+
+    public interface IInt
+    {
+        public IReadOnlyReactiveProperty<int> Value { get; }
+    }
